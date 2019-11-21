61ea1a36c5449d9096dd2cc44adcc1138cc2370f
domgdsman <dominik.gadesmann@gmail.com>
2019-11-21 14:39:09 +0100

    first initial setup

---
+++ final_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace final_project
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
