b7b60adee269aa683bf16d347c3ead41dd061216
domgdsman <dominik.gadesmann@gmail.com>
2019-11-21 15:07:53 +0100

    environment variable added

---
+++ final_project/ENV.cs
@@ -0,0 +1,12 @@
+﻿namespace final_project
+{
+    public static class ENV
+    {
+        private static readonly string SQLiteURI = "";
+
+        public static string GetURI()
+        {
+            return SQLiteURI;
+        }
+    }
+}
